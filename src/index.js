a8d672a66661b89b15c8e5c676c5b3c3891c3801
EdlaKoushik <koushikedla01@gmail.com>
2025-06-15 16:09:52 +0530

    added CORS and updated envs

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ const app =express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true, // fixed typo: should be lowercase 'credentials'
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true,
 }
 ))
 
